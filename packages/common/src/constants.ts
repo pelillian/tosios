7652cb8f4e82f0e9997df96ef1a5c29e386491c5
pelillian <plillian@usc.edu>
2020-05-29 13:31:52 -0700

    Reskinning

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_TITLE = 'TOSIOS';
+export const APP_TITLE = 'Dorado';
 
 // General
 export const WS_PORT = 3001;
@@ -20,7 +20,7 @@ export const GAME_DURATION = 1000 * 90; // 90 seconds
 export const GAME_MODES = ['deathmatch', 'team deathmatch'];
 
 // Background
-export const BACKGROUND_COLOR = '#25131A';
+export const BACKGROUND_COLOR = '#dddddd';
 
 // Tile (rectangle)
 export const TILE_SIZE = 32;
@@ -32,7 +32,7 @@ export const PLAYER_MAX_LIVES = 3;
 export const PLAYER_WEAPON_SIZE = 12; // The bigger, the further away a bullet will be shot from.
 
 // Monster
-export const MONSTERS_COUNT = 3;
+export const MONSTERS_COUNT = 0;
 export const MONSTER_SIZE = 32;
 export const MONSTER_SPEED_PATROL = 0.75;
 export const MONSTER_SPEED_CHASE = 1.25;
@@ -45,7 +45,7 @@ export const MONSTER_PATROL_DURATION_MAX = 3000;
 export const MONSTER_ATTACK_BACKOFF = 3000;
 
 // Props (rectangle)
-export const FLASKS_COUNT = 3;
+export const FLASKS_COUNT = 0;
 export const FLASK_SIZE = 24;
 
 // Bullet (circle)
